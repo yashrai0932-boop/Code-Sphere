61324ea4949716fd1ec27ca15ada741c4e3c7d13
yashrai0932-boop <yashrai0932@gmail.com>
2026-05-22 21:31:42 +0530

    Delete add_block_feature.sql

--- add_block_feature.sql
+++
@@ -1,5 +0,0 @@
--- Add is_blocked column to profiles table
-ALTER TABLE public.profiles ADD COLUMN IF NOT EXISTS is_blocked BOOLEAN DEFAULT FALSE;
-
--- Reload schema cache
-NOTIFY pgrst, 'reload schema';
